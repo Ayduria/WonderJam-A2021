a95baf536a047121fb79f7388b544d339e30e666
Christopher Harvey <webharveyc@gmail.com>
2021-10-09 13:23:23 -0400

    Programmation du endless runner

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
 
-    public GameObject platform;
     public Transform generationPoint;
     private float distanceBetween = 0;
+    public List<GameObject> prefabList = new List<GameObject>();
 
     private float platformWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if(transform.position.x < generationPoint.position.x){
+
+            int prefabIndex = UnityEngine.Random.Range(0,prefabList.Count);
+            Debug.Log(prefabIndex);
+            platformWidth = prefabList[prefabIndex].GetComponent<BoxCollider2D>().size.x;
+
             Vector3 platformPosition = transform.position;
-            platformPosition.x += platformWidth / 2;
-            Debug.Log(platformPosition.x);
-            Instantiate (platform, platformPosition, transform.rotation);
+            platformPosition.x += 0;
+            Instantiate (prefabList[prefabIndex], platformPosition, transform.rotation);
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
         }
     }
