86f1498aa5317415a48838ec67ad6e0c9929b7e9
Ann-Laurie <lapointeannlaurie@gmail.com>
2021-10-10 13:46:39 -0400

    donee

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public SceneLoader sceneLoader;
+    public AudioMixer audioMixer;
+    private float volume;
+
+    private void Awake()
+    {
+        volume = PlayerPrefs.GetFloat("volume", 0.75f);
+    }
+
+    private void Start()
+    {
+        audioMixer.SetFloat("MainVol", Mathf.Log10(volume) * 20);
+        GameObject.FindGameObjectWithTag("Music").GetComponent<Music>().PlayMusic();
+    }
 
     public void PlayGame()
     {
